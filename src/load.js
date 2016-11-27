be03e071073f1fe7d75bf8d9c0ccd5750f3d313a
Dewitt Buckingham <dj729@comcast.net>
2016-11-26 16:10:09 -0800

    Google Card

--- src/load.js
+++ src/load.js
@@ -12,23 +12,24 @@ $(() => {
         .find('span')
         .text('Connected');
 
-      const card = $('<div></div>')
+      $('.card-deck')
+        .append('<div class="card"></div>')
+        .find('.card:last-child')
         .addClass('card')
         .append('<div class="card-block"></div>')
-        .last('.card-block')
+        .find('.card-block')
         .append('<h4 class="card-title"><i class="fa fa-github"></i> GitHub</h4>')
         .append(`<h6 class="card-subtitle text-muted">Last updated ${moment(github.lastUpdated).fromNow()}</h6>`)
         .closest('.card')
         .append('<div class="card-block"></div>')
-        .last('.card-block')
+        .find('.card-block')
+        .last()
         .append('<p class="card-text"></p>')
-        .last('.card-text')
+        .find('.card-text')
         .append(`<span><a href="${github.profileUrl}"><b>${github.username}</b></a></span><br />`)
         .append(`<span>${github._json.total_private_repos + github._json.public_repos} Repositories</span><br />`)
         .append(`<span>${github._json.followers} Followers</span><br />`)
         .append(`<span>Joined ${moment(github._json.created_at).fromNow()}</span><br />`);
-
-      $('.card-deck').append(card);
     }
 
     if (google) {
@@ -37,6 +38,24 @@ $(() => {
         .removeClass('btn-lg')
         .find('span')
         .text('Connected');
+
+      $('.card-deck')
+        .append('<div class="card"></div>')
+        .find('.card:last-child')
+        .addClass('card')
+        .append('<div class="card-block"></div>')
+        .find('.card-block')
+        .append('<h4 class="card-title"><i class="fa fa-google"></i> Google</h4>')
+        .append(`<h6 class="card-subtitle text-muted">Last updated ${moment(google.lastUpdated).fromNow()}</h6>`)
+        .closest('.card')
+        .append('<div class="card-block"></div>')
+        .find('.card-block')
+        .last()
+        .append('<p class="card-text"></p>')
+        .find('.card-text')
+        .append(`<span><a href="${google._json.url}"><b>${google.displayName}</b></a></span><br />`)
+        .append(`<span>"${google._json.tagline}"</span><br />`)
+        .append(`<span>${google._json.aboutMe}</span><br />`);
     }
 
     if (facebook) {
